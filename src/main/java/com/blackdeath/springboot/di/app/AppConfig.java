333c985a0757a996db9e41628faa2180f852f556
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-06 17:26:30 -0600

    Agregado bean con lista de items de factura
    Se crea una lista de ItemFactura como bean de Spring que contiene una lista de ItemFactura
    que se agregan a la Factura

--- src/main/java/com/blackdeath/springboot/di/app/AppConfig.java
+++ src/main/java/com/blackdeath/springboot/di/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.blackdeath.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.blackdeath.springboot.di.app.models.domain.ItemFactura;
+import com.blackdeath.springboot.di.app.models.domain.Producto;
 import com.blackdeath.springboot.di.app.models.services.IServicio;
 import com.blackdeath.springboot.di.app.models.services.MiServicio;
 import com.blackdeath.springboot.di.app.models.services.MiServicioComplejo;
@@ -20,6 +25,17 @@ public class AppConfig {
 	@Bean("miServicioComplejo")
 	public IServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
+	}
+
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems() {
+		Producto producto1 = new Producto("Cámara Sony", 100);
+		Producto producto2 = new Producto("Bicicleta Bianchi aro 26", 200);
+
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 4);
+
+		return Arrays.asList(linea1, linea2);
 	}
 
 }
--- src/main/java/com/blackdeath/springboot/di/app/models/domain/Producto.java
+++ src/main/java/com/blackdeath/springboot/di/app/models/domain/Producto.java
@@ -9,6 +9,11 @@ public class Producto {
 	private String nombre;
 	private Integer precio;
 
+	public Producto(String nombre, Integer precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
