ab30865b11a093b3448b601f2302f7d41dce6680
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-09 08:01:47 -0600

    Ejemplos de @PostConstruct y @PreDestroy
    Se agregan métodos inicializar y destruir anotados con @PostConstruct y @PreDestroy respectivamente
    a la clase Factura.
    
    Los métodos anotados con @PostConstructor se ejecutan justo después de instanciar el bean; es como un
    constructor pero manejado por el contenedor de Spring
    
    Los métodos anotados con @PreDestroy se ejecutan justo antes de destruir la instancia del bean. El contenedor de
    Spring maneja los objetos de forma similar a un Singleton y solo hay una instancia en el contexto. Por lo tanto,
    para que se lleve a cabo la lógica del método anotado con @PreDestroy es necesario detener el contexto, es decir,
    apagar el servidor

--- src/main/java/com/blackdeath/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/blackdeath/springboot/di/app/models/domain/Factura.java
@@ -2,6 +2,9 @@ package com.blackdeath.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -19,6 +22,17 @@ public class Factura {
 	private Cliente cliente;
 	@Autowired
 	private List<ItemFactura> items;
+
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" Karim"));
+		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));
+	}
 
 	public String getDescripcion() {
 		return descripcion;
